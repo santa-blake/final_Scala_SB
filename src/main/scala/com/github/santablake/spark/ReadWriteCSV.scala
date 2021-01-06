80d9285b868a2fede3cfcfaaac6d0c5a8785a638
santa-blake <santablake@inbox.lv>
2021-01-06 19:47:10 +0200

    14

--- src/main/scala/com/github/santablake/spark/ReadWriteCSV.scala
+++ src/main/scala/com/github/santablake/spark/ReadWriteCSV.scala
@@ -1,31 +1,34 @@
 package com.github.santablake.spark
 
+import com.github.mrpowers.spark.daria.sql.DariaWriters
 import org.apache.spark.sql.functions.{col, desc, lit, round}
 import org.apache.spark.sql.{SparkSession, functions}
 
 object ReadWriteCSV extends App {
 
-  val session = SparkSession.builder().appName("test").master("local").getOrCreate()
+  // Starting a session and loading given .csv file as dataframe, SQL syntax work
+
+  val session = SparkSession.builder().appName("ReadWriteCSV").master("local").getOrCreate()
 
   println(s"Session started on Spark version ${session.version}")
   val fPath = "./src/resources/stock_prices.csv"
   val df0 = session.read
     .format("csv")
-    .option("inferSchema", "true") // optional but safer
+    .option("inferSchema", "true") // optional but safer making columns
     .option("header", true) // first row for header
     .load(fPath)
 //  df0.printSchema()
 //  df0.show()
 
 
-  // 1. Compute the average daily return of every stock for every date. Print the results to screen
+  // 1. Computing the average daily return of every stock for every date.
+  // // Printing the results to screen in form:
             //  date	      average_return
             //  yyyy-MM-dd	return of all stocks on that date
 
   val marksColumns1 = Array(col("open"), col("high"), col( "low"), col("close"))
   val averageF = marksColumns1
-    .foldLeft(lit(0)){ (x, y) => x+y}/marksColumns1
-    .length
+    .foldLeft(lit(0)){ (x, y) => x+y}/marksColumns1.length
   val marksColumns2 = Array(col("volume"), col("Price(Avg)"))
   val returnF = marksColumns2
     .foldLeft(lit(1)){ (x, y) => x*y}
@@ -43,33 +46,42 @@ object ReadWriteCSV extends App {
     .groupBy("date")
     .agg(functions.sum("average_return")
       .as("days_avg_trade"))
-    .sort("date")
-    .show(false)
+    .sort("date") //ordering by date
 
+    AverageReturnsByDates.show(false)
+
+  // bonus calculation (avg trade by stock)
   val EveryStockDailyCalc = df1
     .select("date", "average_return")
-    .groupBy("date")
+    .groupBy(col("date"))
     .agg(functions.sum("average_return")
       .as("days_avg_trade_by_stock"))
 
   val DailyAverageReturnsEveryStock = df1
     .select("date", "average_return", "ticker")
-    .show()
-// ?how to convert this show mode unit to dataframe for saving at same time?
 
+ DailyAverageReturnsEveryStock.show()
 
   //2. Save the results to the file as Parquet (CSV and SQL are optional)
 
-//      DariaWriters.writeSingleFile(
-//        df = AverageReturnsByDates,
-//        format = "parquet",
-//        sc = session.sparkContext,
-//        tmpFolder = "./src/resources/tmp2",
-//        filename = "./src/resources/stocks.parquet"
-//      )
+  DariaWriters.writeSingleFile(
+    df = AverageReturnsByDates,
+    format = "parquet",
+    sc = session.sparkContext,
+    tmpFolder = "./src/resources/tmp1",
+    filename = "./src/resources/stocks.parquet"
+  )
 
-         // PREVIOUSLY WRITTEN/SAVED IN .CSV FORMAT, 2 files
-//  val tmpPath = "./src/resources/returns.csv"
+  DariaWriters.writeSingleFile(
+    df = AverageReturnsByDates,
+    format = "csv",
+    sc = session.sparkContext,
+    tmpFolder = "./src/resources/tmp2",
+    filename = "./src/resources/stocksDaria.csv"
+  )
+      // previously written/saved versions in .csv format multi-file folder way
+
+  //val tmpPath = "./src/resources/returns.csv"
 
 //  AverageReturnsByDates
 //    .coalesce(1)
@@ -92,6 +104,20 @@ object ReadWriteCSV extends App {
 //  val dir2 = new File(tmpPath2)
 //  dir2.listFiles.foreach(println)
 
+
+  // SQL way, saving as SQL dataframe on screen (output SQL file not saved - this to be done by connecting to SQL DB online)
+
+  df0.createOrReplaceTempView("stock_prices_table")
+
+  val BestAverageReturns = session.sql( "SELECT ticker, (SUM(close * volume)/COUNT(ticker))/1000 AS BestAverageReturns " +
+    "FROM stock_prices_table " +
+    "GROUP BY ticker " +
+    "ORDER BY BestAverageReturns DESC")
+    BestAverageReturns.show()
+
+  println(s"Calculating stock returns by avg price*volume, the best stock returns: ${BestAverageReturns.select(col("ticker")).first()}.")
+
+
   //  3. Which stock was traded most frequently - as measured by closing price * volume - on average?
 
   val marksColumns3 = Array(col("volume"), col("close"))
@@ -101,17 +127,13 @@ object ReadWriteCSV extends App {
     .withColumnRenamed("ticker","stock_name")
     .sort(desc("average_trade"))
 
-//  val AverageTrades = df2
-//    .select("stock_name", "average_trade")
-//    .show()
+  val AverageTrades = df2
+    .select("stock_name", "average_trade")
 
   val TradesByStock = df2
     .groupBy("stock_name")
     .agg(functions.sum("average_trade")
       .as("avg_trade_by_stock"))
     .sort(desc("avg_trade_by_stock"))
-    //.show()
-
   TradesByStock.show()
-
 }
